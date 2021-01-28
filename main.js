90f345409d7768a55e854b7bd6eefb26e8178704
funcococode <funcococode@gmail.com>
2021-01-28 15:40:13 +0530

    Beautiful dropdown menu

---
+++ main.js
@@ -0,0 +1,26 @@
+const togglers = Array.from(document.querySelectorAll(".dropdownBtn"));
+const ddm = document.querySelectorAll(".dropdownMenu");
+
+togglers.forEach(tog => {
+    tog.addEventListener("mouseover", e => {
+        for (let i = 0; i < togglers.length; i++) {
+            if(togglers[i] != tog){
+                document.getElementById(togglers[i].getAttribute("data-menu")).classList.remove("visible");
+            }
+        }
+        document.getElementById(e.target.getAttribute("data-menu")).classList.add("visible")
+    })
+    
+    tog.addEventListener("mouseout", e => {
+        document.getElementById(e.target.getAttribute("data-menu")).classList.remove("visible")
+    })
+})
+
+ddm.forEach(menu => {
+    menu.addEventListener("mouseover",function(e){
+        menu.classList.add("visible");
+    })
+    menu.addEventListener("mouseout",function(e){
+        menu.classList.remove("visible");
+    })
+})
